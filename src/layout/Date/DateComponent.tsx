48843a6d634b53fd9a5f5009ccc9a141c8f96aa0
Ole Martin Handeland <git@olemartin.org>
2025-01-23 15:14:58 +0100

    The date component did not actually handle localization properly, and it hard-coded a norwegian default answer, whereas other components will just output nothing.

--- src/layout/Date/DateComponent.tsx
+++ src/layout/Date/DateComponent.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 
 import cn from 'classnames';
+import { isValid, parseISO } from 'date-fns';
 
 import classes from 'src/app-components/Date/Date.module.css';
 import { DisplayDate } from 'src/app-components/Date/DisplayDate';
 import { getLabelId } from 'src/components/label/Label';
+import { useCurrentLanguage } from 'src/features/language/LanguageProvider';
 import { ComponentStructureWrapper } from 'src/layout/ComponentStructureWrapper';
+import { formatDateLocale } from 'src/utils/formatDateLocale';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
 import type { PropsFromGenericComponent } from 'src/layout';
 
 export const DateComponent = ({ node }: PropsFromGenericComponent<'Date'>) => {
   const { textResourceBindings, value, icon, direction, format } = useNodeItem(node);
+  const language = useCurrentLanguage();
+  const parsedValue = parseISO(value);
+
+  let displayData: string | null = null;
+  try {
+    displayData = isValid(parsedValue) ? formatDateLocale(language, parsedValue, format) : null;
+  } catch (err) {
+    if (value?.trim()) {
+      window.logErrorOnce(`Date component "${node.id}" failed to parse date "${value}":`, err);
+    }
+  }
 
   if (!textResourceBindings?.title) {
-    return (
-      <DisplayDate
-        value={value}
-        format={format}
-      />
-    );
+    return <DisplayDate value={displayData} />;
   }
 
   return (
@@ -31,11 +40,10 @@ export const DateComponent = ({ node }: PropsFromGenericComponent<'Date'>) => {
       }}
     >
       <DisplayDate
-        value={value}
+        value={displayData}
         iconUrl={icon}
         iconAltText={textResourceBindings.title}
         labelId={getLabelId(node.id)}
-        format={format}
       />
     </ComponentStructureWrapper>
   );
